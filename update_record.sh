c23e2202f5ad2c4a50c985e6a810c99da340ffc3
Hajime MIZUNO <mizuno-as@ubuntu.com>
2014-07-19 13:56:38 +0900

    do-mo, Murabito-san. camaraderie!
    Check hxselect using which.
    Changed a variable name.

--- update_record.sh
+++ update_record.sh
@@ -25,7 +25,7 @@
 DNSCURL=./dnscurl.pl           # path of dnscurl.pl
 KEYNAME=your-key-name          # your key name in .aws-secrets
 TTL=300                        # TTL of this record
-ARECORD=www.example.com.       # Target A record
+NAME=www.example.com.          # Target A record
 ZONEID=YOURZONEID              # your zone ID
 HOSTEDZONE=https://route53.amazonaws.com/2013-04-01/hostedzone
 
@@ -54,12 +54,12 @@ EOF
       <Change>
         <Action>DELETE</Action>
         <ResourceRecordSet>
-          <Name>${ARECORD}</Name>
+          <Name>${NAME}</Name>
           <Type>A</Type>
           <TTL>${TTL}</TTL>
           <ResourceRecords>
             <ResourceRecord>
-              <Value>${CURRENTIP}</Value>
+              <Value>${OLD_RR_VALUE}</Value>
             </ResourceRecord>
           </ResourceRecords>
         </ResourceRecordSet>
@@ -71,12 +71,12 @@ EOF
       <Change>
         <Action>CREATE</Action>
         <ResourceRecordSet>
-          <Name>${ARECORD}</Name>
+          <Name>${NAME}</Name>
           <Type>A</Type>
           <TTL>${TTL}</TTL>
           <ResourceRecords>
             <ResourceRecord>
-              <Value>${NEWIP}</Value>
+              <Value>${NEW_RR_VALUE}</Value>
             </ResourceRecord>
           </ResourceRecords>
         </ResourceRecordSet>
@@ -89,45 +89,46 @@ EOF
     rm $XMLFILE
 }
 
-if [ ! -x /usr/bin/hxselect ]; then
+HXSELECT=${HXSELECT:-$(which hxselect)}
+if [ ! -x "$HXSELECT" ]; then
     echo "hxselect not exist."
     echo "Please install html-xml-utils package."
     exit 1
 fi
 
-RESPONSE=$($DNSCURL --keyname $KEYNAME -- -H "Content-Type: text/xml; charset=UTF-8" -X GET "$HOSTEDZONE/$ZONEID/rrset?name=$ARECORD&type=A&maxitems=1" 2>/dev/null | tail -n1)
+RESPONSE=$($DNSCURL --keyname $KEYNAME -- -H "Content-Type: text/xml; charset=UTF-8" -X GET "$HOSTEDZONE/$ZONEID/rrset?name=$NAME&type=A&maxitems=1" 2>/dev/null | tail -n1)
 
-if [ $(echo $RESPONSE | hxselect -c Name) = $ARECORD ]; then
+if [ "$(echo $RESPONSE | $HXSELECT -c Name)" = "$NAME" ]; then
 
-    # When CNAME of $ARECORD already exists.
+    # When CNAME of $NAME already exists.
     # This script finished without doing anything.
     # Because CNAME record is not allowed to coexist with any other data (RFC1912).
-    if [ $(echo $RESPONSE | hxselect -c Type) = "CNAME" ]; then
+    if [ "$(echo $RESPONSE | $HXSELECT -c Type)" = "CNAME" ]; then
         echo "CNAME already exists."
         exit
     fi
 
-    # When $ARECORD exists on Route 53.
+    # When $NAME exists on Route 53.
     #
     # When A record already exists. -> Update the IP address.
     # A record does not exist. -> Create A record.
-    if [ $(echo $RESPONSE | hxselect -c Type) = "A" ]; then
-        CURRENTIP=$(echo $RESPONSE | hxselect -c Value)
+    if [ "$(echo $RESPONSE | $HXSELECT -c Type)" = "A" ]; then
+        OLD_RR_VALUE=$(echo $RESPONSE | $HXSELECT -c Value)
 
         # If record on Route53 is the same as the current IP address.
         # Skip an update process.
-        if [ $NEWIP = $CURRENTIP ]; then
+        if [ "$NEW_RR_VALUE" = "$OLD_RR_VALUE" ]; then
             echo "IP address did not change."
         else
             postxml "update"
         fi
     else
         # Create new record.
-        echo "$ARECORD already exists. but A record does not exist."
+        echo "$NAME already exists. but A record does not exist."
         postxml "create"
     fi
 else
-    # When a record of "$ARECORD" does not exist on Route 53.
+    # When a record of "$NAME" does not exist on Route 53.
     # Create new record.
     postxml "create"
 fi
