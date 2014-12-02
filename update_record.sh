ff4c7ea217884f28f01e60bffb55f7e7bf127824
Hajime MIZUNO <h-mizuno@infiniteloop.co.jp>
2014-12-02 20:23:26 +0900

    version to use awscli

--- update_record.sh
+++ update_record.sh
@@ -10,7 +10,7 @@
 #
 
 # Please edit the following config file for your environment.
-CONFFILE=${1:-./ddns-route53.conf}
+CONFFILE=${1:-~/.ddns-route53.conf}
 
 if [ ! -r "$CONFFILE" ]; then
     echo "$CONFFILE does not exist or unreadable." 1>&2
@@ -19,109 +19,41 @@ fi
 
 source $CONFFILE
 
-# Check ~/.aws-secrets permission
-PERMISSION=$(stat -L -c '%a' .aws-secrets)
-if [ "$PERMISSION" != "600" ]; then
-    $LOGGER "set your credentials mode to 600."
+# Check required commands.
+AWSCLI=${AWSCLI:-$(which aws)}
+if [ ! -x "$AWSCLI" ]; then
+    $LOGGER "aws command not exist." 1>&2
+    $LOGGER "Please install awscli package." 1>&2
     exit 1
 fi
 
-# This function makes XML and POST it.
-#
-# When "update" argument is given,
-# Add processing to delete the existing A record.
-# Otherwise, please give "create" argument.
-# But the argument is not checked.
-function postxml ()
+JSHON=${JSHON:-$(which jshon)}
+if [ ! -x "$JSHON" ]; then
+    $LOGGER "jshon command not exist." 1>&2
+    $LOGGER "Please install jshon package." 1>&2
+    exit 1
+fi
+
+JSONFILE=$(mktemp /tmp/r53-XXXXXX.json)
+cat > $JSONFILE <<EOF
 {
-    local XMLFILE=$(mktemp /tmp/r53-XXXXXX.xml)
-    cat > $XMLFILE <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<ChangeResourceRecordSetsRequest xmlns="https://route53.amazonaws.com/doc/2013-04-01/">
-  <ChangeBatch>
-    <Changes>
-EOF
-    if [ $1 = "update" ]; then
-        $LOGGER "delete existing A record."
-        cat >> $XMLFILE <<EOF
-      <Change>
-        <Action>DELETE</Action>
-        <ResourceRecordSet>
-          <Name>${NAME}</Name>
-          <Type>A</Type>
-          <TTL>${TTL}</TTL>
-          <ResourceRecords>
-            <ResourceRecord>
-              <Value>${OLD_RR_VALUE}</Value>
-            </ResourceRecord>
-          </ResourceRecords>
-        </ResourceRecordSet>
-      </Change>
-EOF
-    fi
-    $LOGGER "create A record."
-    cat >> $XMLFILE <<EOF
-      <Change>
-        <Action>CREATE</Action>
-        <ResourceRecordSet>
-          <Name>${NAME}</Name>
-          <Type>A</Type>
-          <TTL>${TTL}</TTL>
-          <ResourceRecords>
-            <ResourceRecord>
-              <Value>${NEW_RR_VALUE}</Value>
-            </ResourceRecord>
-          </ResourceRecords>
-        </ResourceRecordSet>
-      </Change>
-    </Changes>
-  </ChangeBatch>
-</ChangeResourceRecordSetsRequest>
-EOF
-    $DNSCURL --keyname $KEYNAME -- -H "Content-Type: text/xml; charset=UTF-8" -X POST --upload-file $XMLFILE $HOSTEDZONE/$ZONEID/rrset
-    rm $XMLFILE
+    "Comment": "update a A record for the zone.",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "${NAME}",
+                "Type": "A",
+                "TTL": ${TTL},
+                "ResourceRecords": [
+                    {
+                        "Value": "${NEW_RR_VALUE}"
+                    }
+                ]
+            }
+        }
+    ]
 }
+EOF
 
-HXSELECT=${HXSELECT:-$(which hxselect)}
-if [ ! -x "$HXSELECT" ]; then
-    $LOGGER "hxselect not exist." 1>&2
-    $LOGGER "Please install html-xml-utils package." 1>&2
-    exit 1
-fi
-
-RESPONSE=$($DNSCURL --keyname $KEYNAME -- -H "Content-Type: text/xml; charset=UTF-8" -X GET "$HOSTEDZONE/$ZONEID/rrset?name=$NAME&type=A&maxitems=1" 2>/dev/null | tail -n1)
-
-if [ "$(echo $RESPONSE | $HXSELECT -c Name)" = "$NAME" ]; then
-
-    # When CNAME of $NAME already exists.
-    # This script finished without doing anything.
-    # Because CNAME record is not allowed to coexist with any other data (RFC1912).
-    if [ "$(echo $RESPONSE | $HXSELECT -c Type)" = "CNAME" ]; then
-        $LOGGER "CNAME already exists."
-        exit
-    fi
-
-    # When $NAME exists on Route 53.
-    #
-    # When A record already exists. -> Update the IP address.
-    # A record does not exist. -> Create A record.
-    if [ "$(echo $RESPONSE | $HXSELECT -c Type)" = "A" ]; then
-        OLD_RR_VALUE=$(echo $RESPONSE | $HXSELECT -c Value)
-
-        # If record on Route53 is the same as the current IP address.
-        # Skip an update process.
-        if [ "$NEW_RR_VALUE" = "$OLD_RR_VALUE" ]; then
-            $LOGGER "IP address did not change."
-        else
-            postxml "update"
-        fi
-    else
-        # Create new record.
-        $LOGGER "$NAME already exists. but A record does not exist."
-        postxml "create"
-    fi
-else
-    # When a record of "$NAME" does not exist on Route 53.
-    # Create new record.
-    postxml "create"
-fi
+${AWSCLI} --profile ${KEYNAME} route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file://${JSONFILE}
